5978d2e13145d7ee5e25f7ba278af8ea7a73d443
Humberto Naves <hsnaves@gmail.com>
2016-10-01 18:00:59 -0400

    Added the jQuery tabs, but corrupted the style :-(

--- Group Creater Options/Group_Creater_Options.js
+++ Group Creater Options/Group_Creater_Options.js
@@ -1,15 +1,5 @@
-window.onload=function(){
-
-	var i = 10;
- function stuff() {
- 	alert(i);
- }
-
-
-}
-
-function tabclick{
-var i = 11;
-stuff();
-
-}
+$(function() {
+	$("#tabbed_navbar").tabs();
+	console.log("Hello!");
+	
+});
