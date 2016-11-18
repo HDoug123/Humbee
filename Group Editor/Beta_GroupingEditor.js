6e619f81118c5662d3a203ad6769a3598c359200
Heather L Douglas <HeatherDouglas@heathers-mbp.attlocal.net>
2016-11-17 22:31:03 -0800

    the bottom bar of the group editor is not displaying

---
+++ Group Editor/Beta_GroupingEditor.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('.toggle-btn').click(function() {
+	  $('.toggle-btn-active').removeClass('toggle-btn-active');
+	  $(this).addClass("toggle-btn-active");
+	});
+	
+});
+
